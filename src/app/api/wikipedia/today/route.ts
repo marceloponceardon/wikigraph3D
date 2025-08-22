1051af3670af3d12baf4d705b32c0e943f9602ff
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-08-22 08:20:27 -0400

    created links and search routes

--- src/app/api/wikipedia/today/route.ts
+++ src/app/api/wikipedia/today/route.ts
@@ -1,33 +1,35 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-	try {
-		const today = new Date();
-		const year = today.getFullYear();
-		const month = String(today.getMonth() + 1).padStart(2, "0");
-		const day = String(today.getDate()).padStart(2, "0");
+  try {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate()).padStart(2, "0");
 
-		const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${month}/${day}`;
-		
-		const res = await fetch(url, {
-			headers: {
-				Authorization: `Bearer ${process.env.WIKIMEDIA_ACCESS_TOKEN}`,
-			},
+    const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${month}/${day}`;
+
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.WIKIMEDIA_ACCESS_TOKEN}`,
+        "User-Agent": `${process.env.APP_NAME} (${process.env.CONTACT})`,
+      },
     });
 
-		if (!res.ok) {
-			console.log("error: !res.ok");
-			return NextResponse.json({ title: "Error fetching article" });
-		}
+    if (!res.ok) {
+      console.log("error: !res.ok");
+      return NextResponse.json({ title: "Error fetching article" });
+    }
 
-		const data = await res.json();
-		const todayArticle = data.tfa?.title || "No article today";
+    const data = await res.json();
+    const todayArticle = data.tfa?.displaytitle || "No article today";
+    console.log(data.tfa);
 
-		console.log(todayArticle);
-		return NextResponse.json({ title: todayArticle });
-	} catch (err) {
-		console.log("error: " + err);
-		return NextResponse.json({ title: "Error fetching article" });
-		console.err(err);
-	}
+    console.log(todayArticle);
+    return NextResponse.json({ title: todayArticle });
+  } catch (err) {
+    console.log("error: " + err);
+    return NextResponse.json({ title: "Error fetching article" });
+    console.err(err);
+  }
 }
