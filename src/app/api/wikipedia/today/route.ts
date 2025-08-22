dba059346905b42a54ddb4bc729a9c6fb62744b2
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-08-22 08:42:19 -0400

    Added more data to the response

--- src/app/api/wikipedia/today/route.ts
+++ src/app/api/wikipedia/today/route.ts
@@ -24,9 +24,17 @@ export async function GET() {
     const data = await res.json();
     const title = data.tfa?.displaytitle || "No article today";
     const thumbnail = data.tfa?.thumbnail || "No thumbnail found";
+    const content_urls = data.tfa?.content_urls || "No urls found";
+    const description = data.tfa?.description || "No description found";
+    const extract = data.tfa?.extract_html || "No extract found";
 
-    console.log(title, thumbnail);
-    return NextResponse.json({ title: title, thumbnail: thumbnail });
+    return NextResponse.json({
+      title: title,
+      thumbnail: thumbnail,
+      content_urls: content_urls,
+      description: description,
+      extract: extract,
+    });
   } catch (err) {
     console.log("error: " + err);
     return NextResponse.json({ title: "Error fetching article" });
