70fba14ac43f67dba55b3998f366d6459e3f61f9
Mars Ponce <marcelo.ponce.ardon@gmail.com>
2025-08-27 12:52:14 -0400

    Implemented basic funcitonality for /api/wikiepdia/links endpoint

---
+++ src/app/api/wikipedia/links/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export async function GET(req, {}) {
+  try {
+    const { searchParams } = new URL(req.url);
+    //		const title = searchParams.get("title");
+    const id = searchParams.get("id");
+    const limit = searchParams.get("limit") || "max"; // default limit is 10
+
+    const url = `https://en.wikipedia.org/w/api.php?action=query&generator=links&pageids=${id}&format=json&gpllimit=${limit}`;
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.WIKIMEDIA_ACCESS_TOKEN}`,
+        "User-Agent": `${process.env.APP_NAME} (${process.env.CONTACT})`,
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    if (!res.ok) {
+      console.error("res!.ok");
+      return NextResponse.json(
+        { error: `Error finding links for title=${title}` },
+        { status: 500 },
+      );
+    }
+
+    return NextResponse.json({
+      links: data.query.pages,
+      continue: data.continue,
+    });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
